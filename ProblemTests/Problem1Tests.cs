a06a6de4f1ba4105e80013ad544dbdeb7e76d8c0
mslancashire <mslancashire@gmail.com>
2018-05-07 19:18:31 +0100

    Finished problems 1 & 2 in brute force fashion.

---
+++ ProblemTests/Problem1Tests.cs
@@ -0,0 +1,57 @@
+using Problems;
+using System.Linq;
+using Xunit;
+
+namespace ProblemTests
+{
+    public class Problem1Tests
+    {
+        [Fact]
+        public void RunProblem1Example_should_return_correct_result()
+        {
+            // arrange
+            var upper = 10;
+            var multiples = new int[] { 3, 5 };
+
+            var sut = new Solution0001();
+
+            // act
+            var natualNumbers = sut.FindMultiples(multiples, upper);
+
+            // assert
+            Assert.Equal(23, natualNumbers.Sum());
+        }
+
+        [Fact]
+        public void RunProblem1_should_return_correct_result()
+        {
+            // arrange
+            var upper = 1000;
+            var multiples = new int[] { 3, 5 };
+
+            var sut = new Solution0001();
+
+            // act
+            var natualNumbers = sut.FindMultiples(multiples, upper);
+
+            // assert
+            Assert.Equal(233168, natualNumbers.Sum());
+        }
+
+        [Fact]
+        public void RunProblem1_should_return_correct_number()
+        {
+            // arrange
+            var upper = 1000;
+            var multiples = new int[] { 3, 5 };
+
+            var sut = new Solution0001();
+
+            // act
+            var sum = sut.SumOfMultiples(multiples, upper);
+
+            // assert
+            Assert.Equal(233168, sum);
+        }
+    }
+}

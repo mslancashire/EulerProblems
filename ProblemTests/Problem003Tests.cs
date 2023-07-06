7f1417aa0f1abaa7721c0c8ab45dcab31305434f
marc.lancashire <mslancashire@gmail.com>
2023-07-06 16:06:59 +0100

    Solution to Problem 3.

---
+++ ProblemTests/Problem003Tests.cs
@@ -0,0 +1,120 @@
+﻿using FluentAssertions;
+using Problems.Problem003;
+
+namespace ProblemTests
+{
+    public class Problem003Tests
+    {
+        [Fact]
+        public void RunProblemTest3_check_example()
+        {
+            // arrange
+            var sut = new Solution003();
+
+            // act
+            var largestPrimeFactor = sut.GatLargestPrimeFactor(13195);
+
+            // assert
+            largestPrimeFactor.Should().Be(29);
+        }
+
+        [Fact]
+        public void RunProblemTest3_check_solution()
+        {
+            // arrange
+            var sut = new Solution003();
+
+            // act
+            var largestPrimeFactor = sut.GatLargestPrimeFactor(600851475143);
+
+            // assert
+            largestPrimeFactor.Should().Be(6857);
+        }
+
+        [Fact]
+        public void FindFactorsOf_should_return_correct_factors_for_10()
+        {
+            // arrange
+            var sut = new Solution003();
+
+            // act
+            var factors = sut.FindFactorsOf(10);
+
+            // assert
+            factors.Should().NotBeEmpty();
+            factors.Should().Equal(1, 2, 5, 10);
+        }
+
+        [Fact]
+        public void FindFactorsOf_should_return_correct_factors_for_10_use_exclude()
+        {
+            // arrange
+            var sut = new Solution003();
+
+            // act
+            var factors = sut.FindFactorsOf(10, false);
+
+            // assert
+            factors.Should().NotBeEmpty();
+            factors.Should().Equal(2, 5);
+        }
+
+        [Fact]
+        public void FindFactorsOf_should_return_correct_factors_for_13195()
+        {
+            // arrange
+            var sut = new Solution003();
+
+            // act
+            var factors = sut.FindFactorsOf(13195);
+
+            // assert
+            factors.Should().NotBeEmpty();
+            factors.Should().Equal(1, 5, 7, 13, 29, 35, 65, 91, 145, 203, 377, 455, 1015, 1885, 2639, 13195);
+        }
+
+        [Fact]
+        public void FindFactorsOf_should_return_correct_factors_for_13195_use_exclude()
+        {
+            // arrange
+            var sut = new Solution003();
+
+            // act
+            var factors = sut.FindFactorsOf(13195, false);
+
+            // assert
+            factors.Should().NotBeEmpty();
+            factors.Should().Equal(5, 7, 13, 29, 35, 65, 91, 145, 203, 377, 455, 1015, 1885, 2639);
+        }
+
+        [Fact]
+        public void FindPrimeFactors_should_return_correct_prime_factors_for_10()
+        {
+            // arrange
+            var sut = new Solution003();
+
+            // act
+            var primeFactors = sut.FindPrimeFactors(10);
+
+            // assert
+            primeFactors.Should().NotBeEmpty();
+            primeFactors.Should().Equal(2, 5);
+        }
+
+        [Fact]
+        public void FindPrimeFactors_should_return_correct_prime_factors_for_13195()
+        {
+            // arrange
+            var sut = new Solution003();
+
+            // act
+            var primeFactors = sut.FindPrimeFactors(13195);
+
+            // assert
+            primeFactors.Should().NotBeEmpty();
+            primeFactors.Should().Equal(5, 7, 13, 29);
+        }
+
+
+    }
+}
---
+++ Problems/Problem003/Solution003.cs
@@ -0,0 +1,54 @@
+﻿namespace Problems.Problem003
+{
+    /// <summary>
+    /// The price factors of 13195 are 5, 7, 13 and 29.
+    /// 
+    /// What is the largest prime factor of the number 600851475143?
+    /// </summary>
+    public class Solution003
+    {
+        public long GatLargestPrimeFactor(long number)
+        {
+            return FindPrimeFactors(number).OrderDescending().First();
+        }
+
+        public IEnumerable<long> FindPrimeFactors(long number)
+        {
+            var factors = FindFactorsOf(number, false);
+
+            var resolvedFactors = factors.ToDictionary(k => k, v => FindFactorsOf(v, false));
+
+            var primeFactors = resolvedFactors.Where(rf => rf.Value.Any() == false);
+
+            return primeFactors.Select(pf => pf.Key);
+        }
+
+        public IEnumerable<long> FindFactorsOf(long number, bool includeDivisonOfOne = true)
+        {
+            long ending = (long) Math.Sqrt(number);
+            
+            var factors = new List<long>();
+
+            for (int factor = 1; factor <= ending; factor++)
+            {
+                if (number % factor == 0)
+                {
+                    factors.Add(factor);
+
+                    if (factor != number / factor)
+                    {
+                        factors.Add(number / factor);
+                    }
+                }
+            }
+
+            if (includeDivisonOfOne == false)
+            {
+                factors.Remove(1);
+                factors.Remove(number);
+            }
+
+            return factors.Order();
+        }
+    }
+}

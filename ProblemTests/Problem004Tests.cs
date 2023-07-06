a72fa06fc67dbd2ec9cceea62f36855b95301392
marc.lancashire <mslancashire@gmail.com>
2023-07-06 18:47:52 +0100

    Solution to problem 4.

---
+++ ProblemTests/Problem004Tests.cs
@@ -0,0 +1,135 @@
+﻿using FluentAssertions;
+using Problems.Problem004;
+
+namespace ProblemTests
+{
+    public class Problem004Tests
+    {
+        [Fact]
+        public void RunProblemTest4_check_example()
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act
+            var largestPalindrome = sut.GetLargestPalindrome(2);
+
+            // assert
+            largestPalindrome.Should().Be(9009);
+        }
+
+        [Fact]
+        public void RunProblemTest4_check_solution_mine()
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act
+            var largestPalindrome = sut.GetLargestPalindrome(3);
+
+            // assert
+            largestPalindrome.Should().Be(906609);
+        }
+
+        [Fact]
+        public void RunProblemTest4_check_solution_v1()
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act
+            var largestPalindrome = sut.GetLargestPalindrome_comment_version_1();
+
+            // assert
+            largestPalindrome.Should().Be(906609);
+        }
+
+        [Fact]
+        public void IsPalindrome_should_return_false_when_text_is_empty()
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act
+            var isPalindrome = sut.IsPalindrome("");
+
+            // assert
+            isPalindrome.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsPalindrome_should_throw_execption_when_text_contains_whitesapce()
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act // assert
+            Assert.Throws<Exception>(() => sut.IsPalindrome("sdfs sds"));
+        }
+
+        [Fact]
+        public void IsPalindrome_should_return_false_when_text_is_not_a_palindrome()
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act
+            var isPalindrome = sut.IsPalindrome("dfdsfs");
+
+            // assert
+            isPalindrome.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("a")]
+        [InlineData("aa")]
+        [InlineData("aba")]
+        [InlineData("aaaa")]
+        [InlineData("cabac")]
+        public void IsPalindrome_should_return_true_when_text_is_a_palindrome(string textToCheck)
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act
+            var isPalindrome = sut.IsPalindrome(textToCheck);
+
+            // assert
+            isPalindrome.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(1, 9)]
+        [InlineData(2, 99)]
+        [InlineData(3, 999)]
+        [InlineData(4, 9999)]
+        public void GenerateMaxNumber_should_generate_a_max_number_of_lenght_of_provided_digits(short digits, int expectedNumber)
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act
+            var generatedMaxNumber = sut.GenerateMaxNumber(digits);
+
+            // assert
+            generatedMaxNumber.Should().Be(expectedNumber);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 10)]
+        [InlineData(3, 100)]
+        [InlineData(4, 1000)]
+        public void GenerateMinNumber_should_generate_a_min_number_of_lenght_of_provided_digits(short digits, int expectedNumber)
+        {
+            // arrange
+            var sut = new Solution004();
+
+            // act
+            var generatedMaxNumber = sut.GenerateMinNumber(digits);
+
+            // assert
+            generatedMaxNumber.Should().Be(expectedNumber);
+        }
+    }
+}
---
+++ Problems/Problem004/Solution004.cs
@@ -0,0 +1,126 @@
+﻿namespace Problems.Problem004
+{
+    /// <summary>
+    /// A palindromic number reads the same both ways. The largest palindrome made from the product to two 2-digit numbers is 9009 = 91 x 99.
+    /// 
+    /// Find the largest palindrome made from the product of two 3-digit numbers.
+    /// </summary>
+    public class Solution004
+    {
+        public int GetLargestPalindrome_comment_version_1()
+        {
+            int sum;
+            int Largest = -1;
+            string res;
+            bool isPalindrome = true;
+
+            for (int x = 999; x >= 1; x--)
+            {
+                for (int y = 999; y >= 1; y--)
+                {
+                    sum = x * y;
+                    res = sum.ToString();
+                    isPalindrome = true;
+
+                    for (int t = 0; t < res.Length / 2; t++)
+                    {
+                        if (res[t] != res[res.Length - t - 1])
+                        {
+                            isPalindrome = false;
+                        }
+
+                        if (isPalindrome)
+                        {
+                            if (sum > Largest)
+                            {
+                                Largest = sum;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return Largest;
+        }
+
+
+        public int GetLargestPalindrome(short digits)
+        {
+            var startingAt = GenerateMaxNumber(digits);
+            var endingAt = GenerateMinNumber(digits);
+
+            var palindromicProducts = new List<(int Product, int FirstNumber, int SecoundNumber)>();
+
+            for (var i = 0; startingAt - i >= endingAt; i++)
+            {
+                var firstNumber = startingAt - i;
+
+                for (var j = 0; startingAt - j >= endingAt; j++)
+                {
+                    var secondNumber = startingAt - j;
+                    var product = firstNumber * secondNumber;
+
+                    if (IsPalindrome(product.ToString()))
+                    {
+                        palindromicProducts.Add((product, firstNumber, secondNumber));
+                    }
+                }
+            }
+
+            if (palindromicProducts.Any())
+            {
+                var max = palindromicProducts.OrderByDescending(p => p.Product).First();
+
+                Console.WriteLine($"{max.FirstNumber} x {max.SecoundNumber}");
+
+                return max.Product;
+            }
+
+            throw new Exception($"A palindromic number was not found from the product start at {startingAt} and ending at {endingAt}.");
+        }
+
+        public int GenerateMaxNumber(short numOfDigits)
+        {
+            int generatedNumber = (int)(Math.Pow(10, numOfDigits) - 1);
+
+            return generatedNumber;
+        }
+
+        public int GenerateMinNumber(short numOfDigits)
+        {
+            int generatedNumber = (int)(Math.Pow(10, numOfDigits - 1));
+
+            return generatedNumber;
+        }
+
+        public bool IsPalindrome(string textToCheck)
+        {
+            if (string.IsNullOrWhiteSpace(textToCheck))
+            {
+                return false;
+            }
+
+            if (textToCheck.Any(c => char.IsWhiteSpace(c)))
+            {
+                throw new Exception($"{nameof(textToCheck)} must not contain any white space.");
+            }
+
+            for (var i = 0; i < textToCheck.Length; i++)
+            {
+                var endIndex = (textToCheck.Length - 1) - i;
+
+                if (i > endIndex)
+                {
+                    break;
+                }
+
+                if (textToCheck[i] != textToCheck[endIndex])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

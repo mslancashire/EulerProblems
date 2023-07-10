542a40fa67a736d1dd4794b3aca72407bca2af76
marc.lancashire <mslancashire@gmail.com>
2023-07-10 17:11:51 +0100

    Add solution to problem 6.

---
+++ ProblemTests/Problem006Tests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Problems.Problem006;
+
+namespace ProblemTests
+{
+    public class Problem006Tests
+    {
+        [Fact]
+        public void GetDifferenceBetweenSumOfSqauresAndSqaureOfSum_check_example()
+        {
+            // arrange
+            var sut = new Solution006();
+
+            // act
+            var difference = sut.GetDifferenceBetweenSqauresAndSumOfSquares(1, 10);
+
+            // assert
+            difference.Should().Be(2640D);
+        }
+
+        [Fact]
+        public void GetDifferenceBetweenSumOfSqauresAndSqaureOfSum_check_solution()
+        {
+            // arrange
+            var sut = new Solution006();
+
+            // act
+            var difference = sut.GetDifferenceBetweenSqauresAndSumOfSquares(1, 100);
+
+            // assert
+            difference.Should().Be(25164150);
+        }
+
+        [Fact]
+        public void CalculateSumOfSquares_should_return_385_when_using_from_1_to_10()
+        {
+            // arrange
+            var sut = new Solution006();
+
+            // act
+            var sumOfSqaures = sut.CalculateSumOfSquares(Enumerable.Range(1, 10));
+
+            // assert
+            sumOfSqaures.Should().Be(385);
+        }
+
+        [Fact]
+        public void CalculateSqaureRootOfSum_should_return_3025_when_using_from_1_to_10()
+        {
+            // arrange
+            var sut = new Solution006();
+
+            // act
+            var sumOfSqaures = sut.CalculateSqaureRootOfSum(Enumerable.Range(1, 10));
+
+            // assert
+            sumOfSqaures.Should().Be(3025);
+        }
+    }
+}
---
+++ Problems/Problem006/Solution006.cs
@@ -0,0 +1,38 @@
+﻿namespace Problems.Problem006
+{
+    /// <summary>
+    /// The sum of the squares of the first ten natural numbers is,
+    ///     2 sq + 2 sq + ... 10 sq = 385.
+    /// 
+    /// The square of the sim of the first ten natural numbers is,
+    ///     (1 + 2 + ... + 10)  sq = 55 sq = 3025
+    /// 
+    /// Hence the difference between the sim of the squares of the first natural number and the square of the sum is
+    /// 3025 - 385 = 2640.
+    /// 
+    /// Find the difference between the sum of the squares of the first one hunderes natural numbers and the sqaure of the sum.
+    /// </summary>
+    public class Solution006
+    {
+        public double GetDifferenceBetweenSqauresAndSumOfSquares(int from, int to, double power = 2)
+        {
+            var numbers = Enumerable.Range(from, to);
+
+            double sumOfSqaures = CalculateSumOfSquares(numbers, power);
+
+            var squareRootOfSum = CalculateSqaureRootOfSum(numbers, power);
+
+            return squareRootOfSum - sumOfSqaures;
+        }
+
+        public double CalculateSumOfSquares(IEnumerable<int> numbers, double power = 2)
+        {
+            return numbers.Aggregate(0D, (total, next) => total += Math.Pow(next, power));
+        }
+
+        public double CalculateSqaureRootOfSum(IEnumerable<int> numbers, double power = 2)
+        {
+            return Math.Pow(numbers.Sum(), power);
+        }
+    }
+}

f17f39378940d67aee3a3034c6e1f0d0b7a1a8e2
marc.lancashire <mslancashire@gmail.com>
2023-07-12 18:27:31 +0100

    Add solution to problem 9.

---
+++ ProblemTests/Problem009Tests.cs
@@ -0,0 +1,111 @@
+﻿using FluentAssertions;
+using Problems.Problem009;
+
+namespace ProblemTests
+{
+    public class Problem009Tests
+    {
+        [Fact]
+        public void CalculateProductOfSinglePythagoranTriplet_check_example()
+        {
+            // arrange
+            var sut = new Solution009();
+
+            // act
+            var (A, B, C, Product) = sut.CalculateProductOfSinglePythagoranTriplet(12);
+
+            // assert
+            A.Should().Be(3);
+            B.Should().Be(4);
+            C.Should().Be(5);
+            Product.Should().Be(60L);
+        }
+
+        [Fact]
+        public void CalculateProductOfSinglePythagoranTriplet_check_solution()
+        {
+            // arrange
+            var sut = new Solution009();
+
+            // act
+            var (A, B, C, Product) = sut.CalculateProductOfSinglePythagoranTriplet(1000);
+
+            // assert
+            A.Should().Be(200);
+            B.Should().Be(375);
+            C.Should().Be(425);
+            Product.Should().Be(31875000L);
+        }
+
+        [Fact]
+        public void CalculateProductOfSinglePythagoranTriplet_check_example_v2()
+        {
+            // arrange
+            var sut = new Solution009();
+
+            // act
+            var (A, B, C, Product) = sut.CalculateProductOfSinglePythagoranTriplet_v2(12);
+
+            // assert
+            A.Should().Be(3);
+            B.Should().Be(4);
+            C.Should().Be(5);
+            Product.Should().Be(60L);
+        }
+
+        [Fact]
+        public void CalculateProductOfSinglePythagoranTriplet_check_solution_v2()
+        {
+            // arrange
+            var sut = new Solution009();
+
+            // act
+            var (A, B, C, Product) = sut.CalculateProductOfSinglePythagoranTriplet_v2(1000);
+
+            // assert
+            A.Should().Be(200);
+            B.Should().Be(375);
+            C.Should().Be(425);
+            Product.Should().Be(31875000L);
+        }
+
+        [Fact]
+        public void IsPythagoranTriplet_should_return_true_for_example()
+        {
+            // arrange
+            var sut = new Solution009();
+
+            // act
+            var isPythagoranTriplet = sut.IsPythagoranTriplet(3, 4, 5);
+
+            // assert
+            isPythagoranTriplet.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsPythagoranTriplet_should_return_false()
+        {
+            // arrange
+            var sut = new Solution009();
+
+            // act
+            var isPythagoranTriplet = sut.IsPythagoranTriplet(3, 6, 5);
+
+            // assert
+            isPythagoranTriplet.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CalculateC_should_return_5_when_a_and_b_are_3_and_4()
+        {
+            // arrange
+            var sut = new Solution009();
+
+            // act
+            var c = sut.CalculateC(3, 4);
+
+            // assert
+            c.Should().Be(5);
+        }
+    }
+}
---
+++ Problems/Problem009/Solution009.cs
@@ -0,0 +1,79 @@
+﻿namespace Problems.Problem009
+{
+    /// <summary>
+    /// A Pythagorean triplet is a set of three natural numbers, $a < b < c$, for which,
+    /// $a^2 + b^2 = c^2.
+    /// 
+    /// For example, $3^2 + 4^2 = 9 + 16 = 25 = 5^2$.
+    /// 
+    /// There exists exactly one Pythagorean triplet for which $a + b + c = 1000$.
+    /// Find the product $abc$.
+    /// </summary>
+    public class Solution009
+    {
+        public (int A, int B, int C, long Product) CalculateProductOfSinglePythagoranTriplet(int target)
+        {
+            var boundary = target / 2;
+
+            bool check(int a, int b, int c) => a + b + c == target;
+
+            var pythagoranTriplets = FindPythagoranTriplets((int) boundary, check);
+
+            var foundTarget = pythagoranTriplets.First(pt => pt.A + pt.B + pt.C == target);
+
+            return (foundTarget.A, foundTarget.B, foundTarget.C, foundTarget.A * foundTarget.B * foundTarget.C);
+        }
+
+        public (int A, int B, int C, long Product) CalculateProductOfSinglePythagoranTriplet_v2(int target)
+        {
+            var boundary = target / 2;
+
+            for (int a = 1; a < boundary; a++)
+            {
+                for (int b = a + 1; b < boundary; b++)
+                {
+                    //if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(target - a - b, 2))
+                    if (a * a + b * b == (target - a - b) * (target - a - b))
+                    {
+                        int c = (int) CalculateC(a, b);
+
+                        return (a, b, c, a * b * c);
+                    }
+                }
+            }
+
+            throw new Exception("Not found pt");
+        }
+
+        public IEnumerable<(int A, int B, int C)> FindPythagoranTriplets(int boundary, Func<int, int, int, bool>? check = null)
+        {
+            var pythagoranTriplents = new List<(int A, int B, int C)>();
+
+            for (int a = 1; a < boundary; a++)
+            {
+                for (int b = a + 1; b < boundary; b++)
+                {
+                    for (int c = b + 1; c < boundary; c++)
+                    {
+                        if (IsPythagoranTriplet(a, b, c) && (check == null || check(a, b, c)))
+                        {
+                            pythagoranTriplents.Add((a, b, c));
+                        }
+                    }
+                }
+            }
+
+            return pythagoranTriplents;
+        }
+
+        public bool IsPythagoranTriplet(int a, int b, int c)
+        {
+            return Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2) && a < b && b < c;
+        }
+
+        public double CalculateC(int a, int b)
+        {
+            return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        }
+    }
+}
